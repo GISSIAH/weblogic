0a5a5c93de59a90184ebc7e2ddc1ff3659338710
GISSIAH <62018768+GISSIAH@users.noreply.github.com>
2022-02-13 14:43:13 +0200

    added theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,55 @@
+import { createTheme } from '@mui/material'
+import { ThemeProvider } from '@mui/system'
 import Layout from '../components/layout'
 import '../styles/globals.css'
 
+
 function MyApp({ Component, pageProps }) {
+  const theme = createTheme({
+    shape: {
+      borderRadius: 10
+    },
+    palette: {
+      primary: {
+        main: "#05445E",
+        light: "#189AB4"
+      },
+      secondary: {
+        main: "#189AB4"
+      },
+
+
+    },
+    typography: {
+      fontFamily: "Roboto, sans-serif",
+      h1: {
+        fontWeight: 600
+      },
+      h2: {
+
+        fontWeight: 600
+      },
+      h3: {
+        fontWeight: 500
+      },
+      h4: {
+        fontWeight: 500
+      },
+      h5: {
+        fontWeight: 400
+      },
+      h6: {
+        fontWeight: 400
+      }
+    }
+  })
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+
   )
 }
 
